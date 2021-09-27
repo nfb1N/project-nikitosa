83f32ad752cdf8d9fc4c3ad454b0ece775be5b07
Nikita <nikolas20052016@gmail.com>
2021-09-27 19:59:01 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace project_nikitosa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Marker mark = new Marker(-12, ConsoleColor.DarkRed);
+          string s = "null";
+          mark.write(s);
+          System.Console.WriteLine($"count of using: {mark.CountOfUsingProp}");
+        }
+
+    }
+}
